dc14e453b9d3a46cfde81671d83404d1928627a9
Vikram Karthikeyan <vkarthik@buffalo.edu>
2019-04-15 12:03:55 -0400

    modified plot boxes function for negative boxes

--- loaders/WiderfaceDataset.py
+++ loaders/WiderfaceDataset.py
@@ -12,13 +12,6 @@ import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 
-RPN_ANCHOR_SCALES = (32, 64, 128, 256, 512)
-
-# Ratios of anchors at each cell (width/height)
-# A value of 1 represents a square anchor, and 0.5 is a wide anchor
-RPN_ANCHOR_RATIOS = [0.5, 1, 2]
-
-
 # https://www.cs.virginia.edu/~vicente/recognition/notebooks/image_processing_lab.html
 class WiderFaceDataset(Dataset):
 
@@ -69,7 +62,8 @@ class WiderFaceDataset(Dataset):
                 # Plot anchors
                 positive_anchors, negative_anchors = get_regions(np.array(Image.open(file_path).convert('RGB'), dtype=np.uint32), 100, bounding_boxes_filtered)
                 print("positive:",len(positive_anchors))
-                self.plot_boxes(file_path, positive_anchors, bounding_boxes_filtered)
+                print("negative:",len(negative_anchors))
+                self.plot_boxes(file_path, positive_anchors, [], bounding_boxes_filtered)
 
                 break
             # break
@@ -124,13 +118,8 @@ class WiderFaceDataset(Dataset):
             results.append(new_box)
 
         return new_im, results
-        
-    def generate_anchors(self, file_name):
-        image = np.array(Image.open(file_name).convert('RGB'), dtype=np.uint32)
-        print(generate_anchors(RPN_ANCHOR_SCALES[0], RPN_ANCHOR_RATIOS, image.shape, 4, 1))
-
     
-    def plot_boxes(self, file, anchors, boxes):
+    def plot_boxes(self, file, positive_anchors, negative_anchors, boxes):
         im = np.array(Image.open(file).convert('RGB'), dtype=np.uint8)
 
         # Create figure and axes
@@ -139,66 +128,21 @@ class WiderFaceDataset(Dataset):
         # Display the image
         ax.imshow(im)
 
-        # Set colors for occlusions
-        box_colors = {
-            0: 'green',
-            1: 'blue',
-            2: 'red'
-        }
-
-        for i, box in enumerate(anchors):
-            rect = patches.Rectangle((box[0],box[1]),box[2],box[3],linewidth=1,edgecolor=box_colors[0],facecolor='none')
-            ax.add_patch(rect)
+        if positive_anchors:
+            for i, box in enumerate(positive_anchors):
+                rect = patches.Rectangle((box[0],box[1]),box[2],box[3],linewidth=1,edgecolor='green',facecolor='none')
+                ax.add_patch(rect)
+        
+        if negative_anchors:
+            for i, box in enumerate(negative_anchors):
+                rect = patches.Rectangle((box[0],box[1]),box[2],box[3],linewidth=1,edgecolor='red', facecolor='none')
+                ax.add_patch(rect)
 
         for i, box in enumerate(boxes):
-            rect = patches.Rectangle((box[0],box[1]),box[2],box[3],linewidth=1,edgecolor=box_colors[2],facecolor='none')
+            rect = patches.Rectangle((box[0],box[1]),box[2],box[3],linewidth=1,edgecolor='blue', facecolor='none')
             ax.add_patch(rect)
 
         plt.show()
-
-
-############################################################
-#  Anchors
-############################################################
-
-def generate_anchors(scales, ratios, shape, feature_stride, anchor_stride):
-    """
-    scales: 1D array of anchor sizes in pixels. Example: [32, 64, 128]
-    ratios: 1D array of anchor ratios of width/height. Example: [0.5, 1, 2]
-    shape: [height, width] spatial shape of the feature map over which
-            to generate anchors.
-    feature_stride: Stride of the feature map relative to the image in pixels.
-    anchor_stride: Stride of anchors on the feature map. For example, if the
-        value is 2 then generate anchors for every other feature map pixel.
-    """
-    # Get all combinations of scales and ratios
-    scales, ratios = np.meshgrid(np.array(scales), np.array(ratios))
-    scales = scales.flatten()
-    ratios = ratios.flatten()
-
-    # Enumerate heights and widths from scales and ratios
-    heights = scales / np.sqrt(ratios)
-    widths = scales * np.sqrt(ratios)
-
-    # Enumerate shifts in feature space
-    shifts_y = np.arange(0, shape[0], anchor_stride) * feature_stride
-    shifts_x = np.arange(0, shape[1], anchor_stride) * feature_stride
-    shifts_x, shifts_y = np.meshgrid(shifts_x, shifts_y)
-
-    # Enumerate combinations of shifts, widths, and heights
-    box_widths, box_centers_x = np.meshgrid(widths, shifts_x)
-    box_heights, box_centers_y = np.meshgrid(heights, shifts_y)
-
-    # Reshape to get a list of (y, x) and a list of (h, w)
-    box_centers = np.stack(
-        [box_centers_y, box_centers_x], axis=2).reshape([-1, 2])
-    box_sizes = np.stack([box_heights, box_widths], axis=2).reshape([-1, 2])
-
-    # Convert to corner coordinates (y1, x1, y2, x2)
-    boxes = np.concatenate([box_centers - 0.5 * box_sizes,
-                            box_centers + 0.5 * box_sizes], axis=1)
-    return boxes
-
 
 def get_regions(features, N, list_bb):
     """
