02de6b16c31f10fc0c09bcc8643357718dd298b3
Vikram Karthikeyan <vkarthik@buffalo.edu>
2019-04-24 20:25:45 -0400

    Added Proposal Target Layer

--- models/config/rpn_config.py
+++ models/config/rpn_config.py
@@ -1,10 +1,13 @@
 PRE_NMS_TOP_N = 10000
 POST_NMS_TOP_N = 2000
-NMS_THRESH = 0.7
+
+NMS_THRESH = 0.9
+
 MIN_SIZE = 2
 BATCHSIZE = 256
 
+
 # IOU >= thresh: positive example
-POSITIVE_OVERLAP = 0.7
+POSITIVE_OVERLAP = 0.9
 # IOU < thresh: negative example
 NEGATIVE_OVERLAP = 0.3
