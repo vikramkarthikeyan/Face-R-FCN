ae1180c8336957f1e3a5f259b410b3516ad7ec6f
Vikram <vkarthik@buffalo.edu>
2019-05-02 03:01:51 +0000

    Fixed make.sh

--- models/make.sh
+++ models/make.sh
@@ -4,17 +4,16 @@
 
 export CUDA_PATH=/usr/local/cuda/
 
-python setup.py build_ext --inplace
+python3 setup.py build_ext --inplace
 rm -rf build
 
 CUDA_ARCH="-gencode arch=compute_61,code=sm_61"
 
 # clean build file
-rm model/psroi_pooling/src/cuda/*.o
+rm psroi_pooling/src/cuda/*.o
 
 
-cd ../../
-cd model/psroi_pooling/src/cuda
+cd psroi_pooling/src/cuda
 echo "Compiling psroi pooling kernels by nvcc..."
 nvcc -c -o psroi_pooling.cu.o psroi_pooling_kernel.cu \
 	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
