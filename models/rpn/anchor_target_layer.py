2ec11cea3cff8b352c55a0ce2a42da945a4dcbe6
Vikram Karthikeyan <vkarthik@buffalo.edu>
2019-05-06 19:36:39 -0400

    Conversion fixwith float

--- models/rpn/anchor_target_layer.py
+++ models/rpn/anchor_target_layer.py
@@ -70,7 +70,7 @@ class _AnchorLayer(nn.Module):
 
         # 3. Get all area overlap for the kept anchors
         # overlaps = self.bbox_overlaps(clipped_boxes, gt_boxes)
-        overlaps = self.bbox_overlaps_vectorized(clipped_boxes, gt_boxes)
+        overlaps = self.bbox_overlaps_vectorized(clipped_boxes.float(), gt_boxes.float())
         overlaps[overlaps == 0] = 1e-10
 
         if cfg.verbose:
