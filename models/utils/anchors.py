b2e670fe1c3f21a415551680675b779e062c1e46
Vikram Karthikeyan <vkarthik@buffalo.edu>
2019-05-12 21:15:10 -0400

    fixed IOU numpy vectorized function

--- models/utils/anchors.py
+++ models/utils/anchors.py
@@ -32,17 +32,29 @@ def generate_anchors(dimensions, box_sizes):
 
 
 def calc_IOU_vectorized_(bboxes1, bboxes2):
-        x11, y11, x12, y12 = np.split(bboxes1, 4, axis=1)
-        x21, y21, x22, y22 = np.split(bboxes2, 4, axis=1)
-        xA = np.maximum(x11, np.transpose(x21))
-        yA = np.maximum(y11, np.transpose(y21))
-        xB = np.minimum(x12, np.transpose(x22))
-        yB = np.minimum(y12, np.transpose(y22))
-        interArea = np.maximum((xB - xA + 1), 0) * np.maximum((yB - yA + 1), 0)
-        boxAArea = (x12 - x11 + 1) * (y12 - y11 + 1)
-        boxBArea = (x22 - x21 + 1) * (y22 - y21 + 1)
-        iou = interArea / (boxAArea + np.transpose(boxBArea) - interArea)
-        return iou
+    x11, y11, x12, y12 = np.split(bboxes1, 4, axis=1)
+
+    x12 = x11 + x12 - 1
+    y12 = y11 + y12 - 1
+
+    x21, y21, x22, y22 = np.split(bboxes2, 4, axis=1)
+
+    x22 = x21 + x22 - 1
+    y22 = y21 + y22 - 1
+    
+    xA = np.maximum(x11, np.transpose(x21))
+    yA = np.maximum(y11, np.transpose(y21))
+    xB = np.minimum(x12, np.transpose(x22))
+    yB = np.minimum(y12, np.transpose(y22))
+    
+    interArea = np.maximum((xB - xA + 1), 0) * np.maximum((yB - yA + 1), 0)
+    
+    boxAArea = (x12 - x11 + 1) * (y12 - y11 + 1)
+    boxBArea = (x22 - x21 + 1) * (y22 - y21 + 1)
+    
+    iou = interArea / (boxAArea + np.transpose(boxBArea) - interArea)
+    
+    return iou
 
 
 def calc_IOU_vectorized_tmp(bboxes1, bboxes2):
